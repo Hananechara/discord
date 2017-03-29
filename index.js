09c67c8f1ea5b6e1e62ec07577e8eb937f2aa21e
CHARA <3674515@ppti-14-503-08.ufr-info-p6.jussieu.fr>
2017-03-29 10:37:28 +0200

    first commit

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ client.on('message', message => {
 });
 client.on('presenceUpdate', function(oldMember, newMember) {
 	console.log(oldMember.presence, '=>', newMember.presence);
-	if  (GuildMember.guild.user.username == 'hanane'){
-		message.reply('bonjour maitre, je suis le bot de MAMADOU et HANANE, que puis-je faire pour vous aujourd hui..');
-	}
-	//console.log(newMember);
+	//if  (GuildMember.guild.user.username == 'hanane'){
+		//message.reply('bonjour maitre, je suis le bot de MAMADOU et HANANE, que puis-je faire pour vous aujourd hui..');
+	//}
+	console.log(newMember);
 });
 client.login(process.env.DISCORD_TOKEN);
  
