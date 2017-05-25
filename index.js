60cc81406b9e959289195de44efd0ce55dab8181
Hananechara <charahanane93@gmail.com>
2017-05-25 22:07:15 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -12,28 +12,28 @@ client.on('message', message => {
 	}
 	if (message.channel.type=='dm' && !message.author.bot){
 		//message.reply('Bienvenue');
+
+
 	if (message.content == '!blague') {
 		axios.get('http://www.chucknorrisfacts.fr/api/get?data=tri:alea;nb:1').then(
 			function(resp){
 				message.reply(resp.data[0].fact);
 		});
 	}
+
 	if (res[0]== '!meteo'){
 		axios.get('http://api.openweathermap.org/data/2.5/weather?q='+res[1]+'&appid=ee30acaddbdacdc273a1606c7ad920d8&units=metric').then(
 			function(resp){
 				message.reply(res[1]+" min "+resp.data.main.temp_min+" max "+resp.data.main.temp_max);
 		});
 	}
+
+
 	if (res[0]== '!image'){
-		console.log( '!image');
-		console.log();
-		axios.get('https://api.imgur.com/3/gallery/random/random/').then(
-				function(resp){			
-			var link=resp[0].data.link;
-			console.log(resp);
-			console.log(link);
-			message.reply(' ',{embed:{url:link,image:{url:link}}});
+		message.reply(' ',{embed:{url:'https://www.smashingmagazine.com/wp-content/uploads/2015/06/10-dithering-opt.jpg',
+		image:{url:'https://www.smashingmagazine.com/wp-content/uploads/2015/06/10-dithering-opt.jpg'}}});
 	});
+
 	}
 		
 	}
