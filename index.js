1756ae05aafc5489990ad1c44c37bfda81f90d5d
Hananechara <charahanane93@gmail.com>
2017-05-28 13:29:41 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -37,13 +37,13 @@ client.on('message', message => {
 		method: 'get',
 		headers: {"authorization": "Client-ID 018d9b095ca5241" },
 		} ).then( function(resp){
-			var k=0;
-			while(! resp.data.data[k].link){
-				console.log(resp.data.data[k]);
+			//var k=0;
+			//while( !resp.data.data[k]){
+				console.log(resp.data);
 				console.log(k.toString());
 				k=k+1
-			}
-			var lienImage= resp.data.data[k].link;
+			//}
+			var lienImage= "https://www.w3schools.com/w3images/fjords.jpg" ;//resp.data.data[2].link;
 			message.reply('',{embed:{url:lienImage,image:{url:lienImage}}});
 				
 		}).catch(console.error);
